9f51f09b3057fad8ea6dcbfa2e78940dddce9e5e
Carlos Naveda <navedacarlosm@gmail.com>
2026-03-11 12:33:13 -0500

    fix name for module llm, create requirements, test 1 real candidate

--- candidate_analyzer.py
+++ candidate_analyzer.py
@@ -2,7 +2,7 @@ import time
 from google_news import search_news
 from jne_api import get_candidates
 from filter_candidates import filter_candidates
-
+from openai_llm_analysis import news_llm_analyzer
 
 # Variables
 president_tag = "PRESIDENTE DE LA REPÚBLICA"
@@ -15,29 +15,32 @@ def main():
     #Capturamos el tiempo de inicio
     start_time = time.time()
 
-    #Obtenemos los candidatos
-    candidates = get_candidates()
+    print(news_llm_analyzer("KEIKO SOFIA FUJIMORI HIGUCHI"))
 
-    #Filtramos los candidatos | Solo presidenciales
-    candidates_presidential = filter_candidates(candidates, president_tag)
 
-    candidates_analyzer_results = []
-
-    for c in candidates_presidential:
-
-        news = search_news(c["full_name"])    # Obtenemos las noticias riesgosas del candidato
-
-        candidates_analyzer_results.append({
-            "full_name": c["full_name"],
-            "dni" : c['dni'],
-            "sex": c['sex'],
-            "apply_position" : c['apply_position'],
-            "political_party": c['political_party'],
-            "news": news,
-        })
-
-    for a in candidates_analyzer_results:
-        print(a)
+    # #Obtenemos los candidatos
+    # candidates = get_candidates()
+    #
+    # #Filtramos los candidatos | Solo presidenciales
+    # candidates_presidential = filter_candidates(candidates, president_tag)
+    #
+    # candidates_analyzer_results = []
+    #
+    # for c in candidates_presidential:
+    #
+    #     news = search_news(c["full_name"])    # Obtenemos las noticias riesgosas del candidato
+    #
+    #     candidates_analyzer_results.append({
+    #         "full_name": c["full_name"],
+    #         "dni" : c['dni'],
+    #         "sex": c['sex'],
+    #         "apply_position" : c['apply_position'],
+    #         "political_party": c['political_party'],
+    #         "news": news,
+    #     })
+    #
+    # for a in candidates_analyzer_results:
+    #     print(a)
 
     # Capturamos el tiempo de fin
     end_time = time.time()
--- openai_llm_analysis.py
+++ openai_llm_analysis.py
@@ -1,8 +1,11 @@
 from openai import OpenAI
-
+from dotenv import load_dotenv
+import os
 from google_news import *
+import json
 
-client = OpenAI()
+load_dotenv()
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 def news_llm_analyzer(candidate):
 
@@ -10,17 +13,24 @@ def news_llm_analyzer(candidate):
     news = search_news(candidate)
     news_formatted = format_news(news)
 
+    #Mostramos las noticias que analizará
+    print(f"""
+Contexto de Noticias
+{news_formatted}
+"""
+         )
+
     #Armamos el prompt
     prompt = f"""
 Eres un analista neutral que evalúa riesgos legales y controversias de candidatos políticos del Perú.
 Debes analizar el siguiente candidato utilizando ÚNICAMENTE las noticias proporcionadas en el contexto.
 
 Candidato:
-Nombre: {candidate["full_name"]}
-DNI: {candidate["dni"]}
-Sexo: {candidate["sex"]}
-Cargo al que postula: {candidate["apply_position"]}
-Partido político: {candidate["political_party"]}
+Nombre: "KEIKO SOFIA FUJIMORI HIGUCHI"
+DNI: 10001088
+Sexo: FEMENINO
+Cargo al que postula: PRESIDENTE DE LA REPÚBLICA
+Partido político: FUERZA POPULAR
 
 Contexto (titulares de noticias):
 {news_formatted}
@@ -52,7 +62,7 @@ Clasificación de riesgo:
 
 Responde SOLO en JSON con esta estructura exacta:
 
-{
+{{
 "risk_level": "green | orange | red",
 "clean": true/false/null,
 "legal_issues": true/false/null,
@@ -62,11 +72,39 @@ Responde SOLO en JSON con esta estructura exacta:
 ],
 "summary": "explicación corta y neutral del análisis",
 "confidence": 0.0-1.0
-}
+}}
 """
     response = client.responses.create(
         model="gpt-4.1-mini",
         input=prompt
     )
 
-    return response.output_text
+    #Mostramos el costo del uso del LLM
+    show_usage(response.usage)
+
+    #Formateamos el resultado a JSON
+    try:
+        result = json.loads(response.output_text.strip())
+    except json.decoder.JSONDecodeError as e:
+        print("Error:", e)
+        print("Output:", response.output_text)
+        return None
+
+    return result
+
+
+def show_usage(usage):
+
+    input_tokens = usage.input_tokens
+    output_tokens = usage.output_tokens
+    total_tokens = usage.total_tokens
+
+    # precios gpt-4.1-mini
+    input_cost = input_tokens * 0.40 / 1_000_000
+    output_cost = output_tokens * 1.60 / 1_000_000
+    total_cost = input_cost + output_cost
+
+    print("Input tokens:", input_tokens)
+    print("Output tokens:", output_tokens)
+    print("Total tokens:", total_tokens)
+    print(f"Total Cost: ${total_cost:.6f}")
